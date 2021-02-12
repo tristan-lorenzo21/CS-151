72e0ea968022e7a7c8c007ef9ed48d0ed964b221
tristan-lorenzo21 <tristan.lorenzo21@gmail.com>
2021-02-11 19:15:06 -0800

    Created Student class with 3 instance variables and a constructor

---
+++ src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    String name;
+    int year;
+    String major;
+
+    Student(String name, int year, String major) {
+        this.name = name;
+        this.year = year;
+        this.major = major;
+    }
+}
