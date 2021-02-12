64cf6a3c2a9401457e0bf0f4ee7003e70b124d55
tristan-lorenzo21 <tristan.lorenzo21@gmail.com>
2021-02-11 20:34:29 -0800

    Added javadocs for the Main and Student class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+/**
+ *  Main class for displaying the student's name
+ */
+
 public class Main {
+
+    /**
+     * Creates a new Student object and passing in "Tristan Lorenzo" as the parameter
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         Student tristan = new Student("Tristan Lorenzo");
         System.out.println(tristan.name);
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,6 +1,18 @@
+/**
+ *  A class for declaring a student's name
+ */
+
 public class Student {
+    /**
+     * Instance variable for the student's name
+     */
     String name;
 
+    /**
+     * Constructs a Student object that initializes the name of the student
+     *
+     * @param name the name of the student that is submitting this assignment
+     */
     Student(String name) {
         this.name = name;
     }
